fe40e9aa6f246155f03ac92454f8bf6fcdf8ea1a
njf042 <njf042@gmail.com>
2024-08-04 13:07:44 -0700

    Added canvas resizing code.

--- termol/canvas.py
+++ termol/canvas.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 
 class Canvas():
     def __init__(self, char_width, char_height, width, height, aspect_ratio):
@@ -22,12 +23,47 @@ class Canvas():
 
         self.data = [[' '] * char_width for i in range(char_height)]
 
+    def resize_canvas(self, char_width, char_height):
+        '''
+        Resize the canvas. This changes the character width and height,
+        and also scales the "real-world" width in height.
+        Also clears canvas data.
+        '''
+        # If odd, make it even
+        if char_width % 2 == 1:
+            char_width -= 1
+        if char_height % 2 == 1:
+            char_height -= 1
+
+        # Save old values
+        char_width_old = self.char_width
+        char_height_old = self.char_height
+
+        # Update values
+        self.char_width = char_width
+        self.char_height = char_height
+
+        # Scale width and height:
+        scale_x = char_width / char_width_old
+        scale_y = char_height / char_height_old
+
+        # Update the real-world width and height
+        self.width  = self.width * scale_x
+        self.height = self.height * scale_y
+
+        self.min_x = -self.width/2
+        self.max_x = self.width/2
+        self.min_y = -self.height/2
+        self.max_y = self.height/2
+
+        self.data = [[' '] * char_width for i in range(char_height)]
+
     def map_coords(self, x, y):
         '''
         Maps real-world coordinates to ascii-canvas coordinates.
         '''
-        x = int((x - self.min_x) / (self.max_x - self.min_x) * self.char_width)
-        y = int((y - self.min_y) / (self.max_y - self.min_y) * self.char_height)
+        x = int(np.round( (x - self.min_x) / (self.max_x - self.min_x) * self.char_width) )
+        y = int(np.round( (y - self.min_y) / (self.max_y - self.min_y) * self.char_height))
         return x, y
     
     def clear(self):
@@ -82,7 +118,8 @@ class MoleculeCanvas(Canvas):
         '''
 
         def bresenham(x1, y1, x2, y2):
-            """Bresenham's Line Algorithm to find the points on a line between two points"""
+            """Bresenham's Line Algorithm to find the points on a line between two points.
+            Currently, this takes a step of length 1 along the path and assumes integer inputs."""
             points = []
             dx = abs(x2 - x1)
             dy = abs(y2 - y1)
@@ -165,7 +202,3 @@ class MoleculeCanvas(Canvas):
                     self.data[charge_y][charge_x+1] = '+' if charge > 0 else '-'
                     self.data[charge_y][charge_x+2] = str(abs(charge))
                     self.data[charge_y][charge_x+3] = ']'
-                    # self[charge_x, charge_y] = '['
-                    # self[charge_x+1, charge_y] = '+' if charge > 0 else '-'
-                    # self[charge_x+2, charge_y] = str(abs(charge))
-                    # self[charge_x+3, charge_y] = ']'
